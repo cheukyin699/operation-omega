43fd115594d3fca96581cb238f96119a00694bb8
Cheuk Yin Ng <chucksys88@gmail.com>
2018-03-09 15:36:37 -0800

    Starts work on raycasting things

---
+++ Assets/Scripts/CheckObject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckObject : MonoBehaviour {
+
+    [SerializeField] private float m_MaxDistance = 0;
+
+    private Camera m_Camera;
+
+	// Use this for initialization
+	void Start () {
+        m_Camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (!Physics.Raycast (m_Camera.transform.position, m_Camera.transform.forward, out hit, m_MaxDistance))
+            return;
+        else {
+            GameObject obj = hit.collider.gameObject;
+            Renderer r = obj.GetComponent<Renderer> ();
+
+            if (r != null) {
+                // Material exists
+                Material[] mats = r.materials;
+                Array.Resize<Material> (ref mats, mats.Length + 1);
+        }
+	}
+}
