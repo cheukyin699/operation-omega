625ee63c17c6393a40d32c7a576d72821cc0e016
Cheuk Yin Ng <chucksys88@gmail.com>
2018-03-14 09:56:36 -0700

    Finishes selection function

--- Assets/Scripts/CheckObject.cs
+++ Assets/Scripts/CheckObject.cs
@@ -7,7 +7,6 @@ public class CheckObject : MonoBehaviour {
 
     [SerializeField] private float m_MaxDistance = 0;
     [SerializeField] private Material m_GlowingMaterial = null;
-    [SerializeField] private Material m_NoneMaterial = null;
 
     private Camera m_Camera;
     private Renderer[] m_SeeingRenders;
@@ -22,6 +21,20 @@ public class CheckObject : MonoBehaviour {
             Debug.LogError ("Error: 'Glow Outline' material not found.");
         }
 	}
+
+    // Reverts existing game object back to the way God intended
+    void Revert () {
+        if (m_SeeingRenders != null) {
+            // Remove all the object's glowing material
+            for (int j = 0; j < m_SeeingRenders.Length; j++) {
+                Material[] ms = m_SeeingRenders [j].materials;
+                int last = ms.Length - 1;
+                ms [last] = null;
+                m_SeeingRenders [j].materials = ms;
+            }
+            m_SeeingRenders = null;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -29,36 +42,23 @@ public class CheckObject : MonoBehaviour {
         RaycastHit hit;
         if (!Physics.Raycast (m_Camera.transform.position, m_Camera.transform.forward, out hit, m_MaxDistance)) {
             // Raycast doesn't hit anything
-            if (m_SeeingRenders != null) {
-                // Remove all the object's glowing material
-                for (int j = 0; j < m_SeeingRenders.Length; j++) {
-                    Material[] ms = m_SeeingRenders[j].materials;
-                    for (int i = 0; i < ms.Length; i++) {
-                        // Revert all glowing materials into 'null'
-                        if (ms [i].Equals(m_GlowingMaterial)) {
-                            ms [i] = m_NoneMaterial;
-                        }
-                    }
-                    m_SeeingRenders[j].materials = ms;
-                }
-            }
+            Revert ();
         } else {
+            // Raycast hits
             Renderer[] rs = hit.collider.gameObject.GetComponentsInChildren<Renderer> ();
 
             if (rs != null && m_SeeingRenders != rs) {
-                // Render exists; take it
+                // Reverts everything, if necessary
+                Revert ();
+                // Renders exists; take it
                 m_SeeingRenders = rs;
 
                 // Do for every material in every render available...
                 for (int j = 0; j < rs.Length; j++) {
                     Material[] ms = rs[j].materials;
-                    for (int i = 0; i < ms.Length; i++) {
-                        // Turn all non-existant materials into the glowing variant
-                        if (ms [i] == m_NoneMaterial) {
-                            ms [i] = m_GlowingMaterial;
-                        }
-                    }
-                    rs[j].materials = ms;
+                    int last = ms.Length - 1;
+                    ms [last] = new Material(m_GlowingMaterial);
+                    rs [j].materials = ms;
                 }
             }
         }
